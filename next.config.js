fdda9f545a814d6bb73c74b38350a799d617fd32
shoaib memon <shoaibattari83@gmail.com>
2023-04-06 02:05:48 +0500

    contect setup run

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
   experimental: {
     appDir: true,
   },
-}
+  env: {
+    CONTENTFUL_SPACE_ID: "efgtsnt47wfz",
+    CONTENTFUL_ACCESS_KEY:"_zfOhzNVVZuMuEefpIOEb7yJS8QNi-oIG5oIDEw-CXc",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
